6a2214f674d94e1b6d7986ee8fcd82dab82ad772
Joseph Artsimovich <joseph.artsimovich@gmail.com>
2015-06-13 12:10:25 +0100

    Introducing a new class: VectorFieldImageView
    It's a debugging view for visualizing a grid of 2D vectors.

---
+++ VectorFieldImageView.cpp
@@ -0,0 +1,135 @@
+/*
+    Scan Tailor - Interactive post-processing tool for scanned pages.
+    Copyright (C) 2015  Joseph Artsimovich <joseph.artsimovich@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "VectorFieldImageView.h"
+#include "VectorFieldImageView.moc"
+#include "ImagePresentation.h"
+#include "ImagePixmapUnion.h"
+#include "InteractionState.h"
+#include "MatMNT.h"
+#include "VecNT.h"
+#include <QPointF>
+#include <QRectF>
+#include <QMouseEvent>
+#include <QPainter>
+#include <QPen>
+#include <QColor>
+#include <stdexcept>
+#include <algorithm>
+#include <stddef.h>
+
+VectorFieldImageView::VectorFieldImageView(
+	QImage const& image, Grid<Vec2f> const& directions, float representative_dir_magnitude)
+:	ImageViewBase(
+		image, ImagePixmapUnion(),
+		ImagePresentation(QTransform(), QRectF(image.rect()))
+	)
+,	m_dragHandler(*this)
+,	m_zoomHandler(*this)
+,	m_directions(directions)
+,	m_imageRect(image.rect())
+,	m_repDirMagnitudeRecip(1.0f / std::max<float>(representative_dir_magnitude, 1e-5f))
+,	m_dirScale(1.f)
+,	m_lastWidgetSize(0, 0)
+,	m_lastZoomLevel(0.0)
+{
+	rootInteractionHandler().makeLastFollower(m_dragHandler);
+	rootInteractionHandler().makeLastFollower(m_zoomHandler);
+	rootInteractionHandler().makeLastFollower(*this);
+
+	setMouseTracking(true);
+}
+
+VectorFieldImageView::~VectorFieldImageView()
+{
+}
+
+void
+VectorFieldImageView::onPaint(QPainter& painter, InteractionState const& interaction)
+{
+	if (interaction.captured()) {
+		return;
+	}
+
+	if (!m_imageRect.contains(m_imageFocalPoint)) {
+		return;
+	}
+
+	if (size() != m_lastWidgetSize || zoomLevel() != m_lastZoomLevel) {
+		// Update m_dirScale.
+		QTransform const w2i(widgetToImage());
+		float const scale_upper_bound = 0.5f * float(std::min(viewport()->width(), viewport()->height()))
+			* std::min(w2i.m11(), w2i.m22()) * m_repDirMagnitudeRecip;
+		float const scale_lower_bound = 30.f * std::max(w2i.m11(), w2i.m22()) * m_repDirMagnitudeRecip;
+		m_dirScale = qBound(scale_lower_bound, m_dirScale, scale_upper_bound);
+		m_lastWidgetSize = size();
+		m_lastZoomLevel = zoomLevel();
+	}
+
+	painter.setRenderHint(QPainter::Antialiasing);
+
+	// Setup for drawing in image coordinates.
+	painter.setWorldTransform(imageToWidget());
+
+	QPen pen(QColor(0, 0, 255));
+	pen.setCosmetic(true);
+	pen.setWidthF(2.5);
+	painter.setPen(pen);
+
+	Vec2f vec(m_directions(m_imageFocalPoint.x(), m_imageFocalPoint.y()));
+	vec *= m_dirScale;
+	QPointF const arrow_tip(m_imageFocalPoint + vec);
+	painter.drawLine(m_imageFocalPoint, arrow_tip);
+	
+	// Now we will be drawing  the > part of the arrow.
+
+	float const sin45 = 0.707106781f;
+	float const cos45 = sin45;
+	
+	Mat22f R;
+	R(0, 0) = cos45;
+	R(1, 1) = cos45;
+	R(0, 1) = sin45;
+	R(1, 0) = -sin45;
+	
+	QLineF arrow_line1(arrow_tip, arrow_tip - R * vec);
+	std::swap(R(0, 1), R(1, 0)); // Rotate the other way.
+	QLineF arrow_line2(arrow_tip, arrow_tip - R * vec);
+
+	// Draw in widget coordinates;
+	painter.setWorldTransform(QTransform());
+	
+	arrow_line1 = imageToWidget().map(arrow_line1);
+	arrow_line2 = imageToWidget().map(arrow_line2);
+	arrow_line1.setLength(6.0);
+	arrow_line2.setLength(6.0);
+
+	painter.drawLine(arrow_line1);
+	painter.drawLine(arrow_line2);
+
+	// Finlly, print the scale of our direction vectors.
+	QString const text(tr("Scale: %1").arg(m_dirScale, 0, 'g', 3));
+	painter.drawText(viewport()->rect(), Qt::AlignBottom|Qt::AlignRight, text);
+}
+
+void
+VectorFieldImageView::onMouseMoveEvent(QMouseEvent* event, InteractionState& interaction)
+{
+	m_imageFocalPoint = widgetToImage().map(event->pos());
+	update();
+}
---
+++ VectorFieldImageView.h
@@ -0,0 +1,54 @@
+/*
+    Scan Tailor - Interactive post-processing tool for scanned pages.
+    Copyright (C) 2015  Joseph Artsimovich <joseph.artsimovich@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef VECTOR_FIELD_IMAGE_VIEW_H_
+#define VECTOR_FIELD_IMAGE_VIEW_H_
+
+#include "ImageViewBase.h"
+#include "InteractionHandler.h"
+#include "DragHandler.h"
+#include "ZoomHandler.h"
+#include "Grid.h"
+#include "VecNT.h"
+#include <QPoint>
+#include <QRect>
+
+class VectorFieldImageView : public ImageViewBase, public InteractionHandler
+{
+	Q_OBJECT
+public:
+	VectorFieldImageView(QImage const& image, Grid<Vec2f> const& directions, float max_dir_magnitude);
+	
+	virtual ~VectorFieldImageView();
+protected:
+	virtual void onPaint(QPainter& painter, InteractionState const& interaction);
+
+	virtual void onMouseMoveEvent(QMouseEvent* event, InteractionState& interaction);
+private:
+	DragHandler m_dragHandler;
+	ZoomHandler m_zoomHandler;
+	Grid<Vec2f> m_directions;
+	QRect m_imageRect;
+	QPoint m_imageFocalPoint;
+	float m_repDirMagnitudeRecip;
+	float m_dirScale;
+	QSize m_lastWidgetSize;
+	double m_lastZoomLevel;
+};
+
+#endif
