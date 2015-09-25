trufanov-nok/scantailor-universal

commit 74b45973ad2e660a3251f4a739130699420c0a2d

Fix a problem with automatic page splitting after fixing page orientation
The problem was the assumption that the top-left corner of the bounding
box of transformed image crop area is always at (0, 0). In "experimental"
branch that's no longer the case.

filters/page_split/PageLayoutEstimator.cpp
@@ -258,9 +258,9 @@ namespace
 class BadTwoPageSplitter
 {
 public:
-	BadTwoPageSplitter(double image_width)
-	: m_imageCenter(0.5 * image_width),
-	m_distFromCenterThreshold(0.6 * m_imageCenter) {}
+	BadTwoPageSplitter(QRectF const& image_rect)
+	: m_imageCenter(image_rect.center().x()),
+	m_distFromCenterThreshold(0.6 * (m_imageCenter - image_rect.left())) {}
 	
 	/**
 	 * Returns true if the line is too close to an edge
@@ -347,7 +347,7 @@ PageLayoutEstimator::tryCutAtFoldingLine(
 		lines.erase(
 			std::remove_if(
 				lines.begin(), lines.end(),
-				BadTwoPageSplitter(virtual_image_rect.width())
+				BadTwoPageSplitter(virtual_image_rect)
 			), lines.end()
 		);
 		return autoDetectTwoPageLayout(lines, virtual_image_rect);

commit aabcfb2d0b1ba1123bf8df17b20286295e277747

Fix another instance of "wrong page split after fixing orientation" problem
The same problem as in 74b4597 caused by the same assumption that no
longer holds in "experimental" branch, just in a different place.

This time we fix it by adjusting the transform so that the assumption
in question actually holds for this particular transform.

filters/page_split/PageLayoutEstimator.cpp
@@ -380,6 +380,9 @@ PageLayoutEstimator::cutAtWhitespace(
 		AffineTransformedImage const downscaled = image.withAdjustedTransform(
 			[](AffineImageTransform& xform) {
 				xform.scaleTo(QSize(3000, 3000), Qt::KeepAspectRatio);
+				xform.translateSoThatPointBecomes(
+					xform.transformedCropArea().boundingRect().topLeft(), QPointF(0.0, 0.0)
+				);
 			}
 		);
 		img = binarize(downscaled, accel_ops);