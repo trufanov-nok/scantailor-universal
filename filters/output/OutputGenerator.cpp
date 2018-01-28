3b0f10471a447ca11dbd6e11a80b546d2d25ff27
Alexander Trufanov <trufanovan@gmail.com>
2018-01-28 17:56:48 +0300

    Fix colorspace format for binary output after dewarp to keep filesize small
    After dewarp additional deskew may be applied. In case of binary output that
    will change image colorspace format from mono to indexed8. As a result output
    filesize will increase significantly.
    The problems is that imageproc::transform() doesn't support binary colorspaces
    and convert image to rgb while BinaryImage::toQImage() always return QImage in
    Format_Mono.
    I don't want to add mono format support into imageproc::transform() so just
    enforce conversion back to mono format for such scans.

--- filters/output/OutputGenerator.cpp
+++ filters/output/OutputGenerator.cpp
@@ -1600,7 +1600,7 @@ OutputGenerator::processWithDewarping(TaskStatus const& status, FilterData const
 //begin of modified by monday2000
 		QImage tmp_image(dewarped_bw_content.toQImage()); 
 		maybe_deskew(&tmp_image, dewarping_mode);		
-		return tmp_image;		
+        return tmp_image.convertToFormat(QImage::Format_Mono);
 //end of modified by monday2000
 	}
 
