696deda6de27cb8ab047b449d3ef0932ec6f8ed2
Alexander Trufanov <trufanovan@gmail.com>
2018-03-09 15:43:37 +0300

    About dlg change + version bump
    to 0.2.x

--- version.h
+++ version.h
@@ -73,7 +73,7 @@
 #define BUILD_MONTH ((BUILD_DATE_IS_BAD) ? 00 : COMPUTE_BUILD_MONTH)
 #define BUILD_DAY   ((BUILD_DATE_IS_BAD) ? 00 : COMPUTE_BUILD_DAY)
 
-#define VERSION "0.0.1"
-#define VERSION_QUAD "0.0.1.0"
+#define VERSION "0.2.0"
+#define VERSION_QUAD "0.2.0.0"
 
 #endif
