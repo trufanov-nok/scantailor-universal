c756bfbfba5912171070e18d2cac642d75f024db
Alexander Trufanov <trufanovan@gmail.com>
2018-03-22 02:20:00 +0300

    OrderByAlignment

--- filters/page_layout/Filter.cpp
+++ filters/page_layout/Filter.cpp
@@ -31,6 +31,7 @@
 #include "CacheDrivenTask.h"
 #include "OrderByWidthProvider.h"
 #include "OrderByHeightProvider.h"
+#include "OrderByAlignment.h"
 #include "Utils.h"
 #ifndef Q_MOC_RUN
 #include <boost/lambda/lambda.hpp>
@@ -66,9 +67,11 @@ Filter::Filter(IntrusivePtr<ProjectPages> const& pages,
 	typedef PageOrderOption::ProviderPtr ProviderPtr;
 
 	ProviderPtr const default_order;
+    ProviderPtr const order_by_alignment(new OrderByAlignment(m_ptrSettings));
 	ProviderPtr const order_by_width(new OrderByWidthProvider(m_ptrSettings));
 	ProviderPtr const order_by_height(new OrderByHeightProvider(m_ptrSettings));
 	m_pageOrderOptions.push_back(PageOrderOption(tr("Natural order"), default_order));
+    m_pageOrderOptions.push_back(PageOrderOption(tr("Order by alignment type"), order_by_alignment));
 	m_pageOrderOptions.push_back(PageOrderOption(tr("Order by increasing width"), order_by_width));
 	m_pageOrderOptions.push_back(PageOrderOption(tr("Order by increasing height"), order_by_height));
 }
---
+++ filters/page_layout/OrderByAlignment.cpp
@@ -0,0 +1,83 @@
+/*
+    Scan Tailor - Interactive post-processing tool for scanned pages.
+    Copyright (C)  Joseph Artsimovich <joseph.artsimovich@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "OrderByAlignment.h"
+#include "Params.h"
+#include "Margins.h"
+#include "PageId.h"
+#include <QSizeF>
+#include <memory>
+#include <assert.h>
+
+namespace page_layout
+{
+
+OrderByAlignment::OrderByAlignment(IntrusivePtr<Settings> const& settings)
+:	m_ptrSettings(settings)
+{
+}
+
+bool
+OrderByAlignment::precedes(
+	PageId const& lhs_page, bool const lhs_incomplete,
+	PageId const& rhs_page, bool const rhs_incomplete) const
+{
+
+    if (lhs_incomplete != rhs_incomplete) {
+        // Pages with question mark go to the bottom.
+        return rhs_incomplete;
+    } else if (lhs_incomplete) {
+        assert(rhs_incomplete);
+        // Two pages with question marks are ordered naturally.
+        return lhs_page < rhs_page;
+    }
+
+    assert(lhs_incomplete == false);
+    assert(rhs_incomplete == false);
+
+	std::auto_ptr<Params> const lhs_params(m_ptrSettings->getPageParams(lhs_page));
+	std::auto_ptr<Params> const rhs_params(m_ptrSettings->getPageParams(rhs_page));
+
+
+    bool lhs_is_null = true;
+    int lhs_alignment;
+	if (lhs_params.get()) {
+        lhs_is_null = lhs_params->alignment().isNull();
+        lhs_alignment = lhs_params->alignment().compositeAlignment();
+	}
+
+    bool rhs_is_null = true;
+    int rhs_alignment;
+    if (rhs_params.get()) {
+        rhs_is_null = rhs_params->alignment().isNull();
+        rhs_alignment = rhs_params->alignment().compositeAlignment();
+    }
+
+    if (lhs_is_null != rhs_is_null) {
+        // Pages with no alignment go to the top.
+        return lhs_is_null;
+    } else if (lhs_is_null) {
+        assert(rhs_is_null);
+        // Two pages with no alignmen are ordered naturally.
+        return lhs_page < rhs_page;
+    }
+	
+    return (! (lhs_alignment < rhs_alignment) );
+}
+
+} // namespace page_layout
