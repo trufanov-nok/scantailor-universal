a72fc7616c242fb845d17b7471f2302bc73d6b2c
Alexander Trufanov <trufanov-nok@users.noreply.github.com>
2018-04-04 22:55:54 +0300

    Set c++0x by default
    otherwise won't compile on old mac

--- cmake/default_cxxflags.cmake
+++ cmake/default_cxxflags.cmake
@@ -2,9 +2,12 @@ IF(MSVC)
 	# If we want reliable stack traces, we need /Oy-
 	# We do it only for RelWithDebInfo, because configuration is used with
 	# the built-in crash reporter.
-	SET(_common "/FS /wd4267")
+	SET(_common "/FS /wd4267 /std=c++0x")
 	SET(CMAKE_CXX_FLAGS_RELEASE_INIT "/MD /O2 /Ob2 /D NDEBUG ${_common}")
 	SET(CMAKE_CXX_FLAGS_DEBUG_INIT "/D_DEBUG /MDd /Zi /Ob0 /Od /RTC1 ${_common}")
 	SET(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "/MD /Zi /O2 /Ob1 /D NDEBUG /Oy- ${_common}")
 	SET(CMAKE_CXX_FLAGS_MINSIZEREL_INIT "/MD /O1 /Ob1 /D NDEBUG ${_common}")
+ELSE()
+	SET(CMAKE_CXX_FLAGS "-std=c++0x")
+	SET(QMAKE_CXX_FLAGS "-std=c++0x")
 ENDIF()
