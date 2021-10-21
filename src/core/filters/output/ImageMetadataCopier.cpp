c1a7d797160c12aef32a19e7e08f2e099c8f3292
Alexander Trufanov <trufanovan@gmail.com>
2021-10-21 15:20:21 +0300

    Fix ICC propagation for non ASCII filenames under Win

--- src/core/filters/output/ImageMetadataCopier.cpp
+++ src/core/filters/output/ImageMetadataCopier.cpp
@@ -6,7 +6,11 @@
 bool
 ImageMetadataCopier::iccProfileDefined(const QString& filename)
 {
+#ifdef _WIN32
+    const Exiv2::Image::AutoPtr src_image = Exiv2::ImageFactory::open(filename.toStdWString());
+#else
     const Exiv2::Image::AutoPtr src_image = Exiv2::ImageFactory::open(filename.toStdString());
+#endif
     if (src_image.get()) {
         src_image->readMetadata();
         return src_image->iccProfileDefined();
@@ -20,13 +24,22 @@ ImageMetadataCopier::copyMetadata(const QString& src_img,
 {
     try
     {
-        const Exiv2::Image::AutoPtr src_image = Exiv2::ImageFactory::open(src_img.toStdString());
+#ifdef _WIN32
+		const Exiv2::Image::AutoPtr src_image = Exiv2::ImageFactory::open(src_img.toStdWString());
+#else
+		const Exiv2::Image::AutoPtr src_image = Exiv2::ImageFactory::open(src_img.toStdString());
+#endif
+
         if (src_image.get()) {
             src_image->readMetadata();
 
             if (src_image->iccProfileDefined()) {
                 src_image->iccProfile();
-                Exiv2::Image::AutoPtr dst_image = Exiv2::ImageFactory::open(dst_img.toStdString());
+#ifdef _WIN32
+				const Exiv2::Image::AutoPtr dst_image = Exiv2::ImageFactory::open(dst_img.toStdWString());
+#else
+				const Exiv2::Image::AutoPtr dst_image = Exiv2::ImageFactory::open(dst_img.toStdString());
+#endif
                 dst_image->readMetadata();
                 dst_image->setIccProfile(*src_image->iccProfile());
                 dst_image->writeMetadata();
