396cd18b9df3ee38e25ff6e3364230bdc97c6aee
Alexander Trufanov <trufanovan@gmail.com>
2023-07-15 22:52:31 +0300

    Fix compilation with libexiv2 newer than 0.28.0
    resolves #131

--- src/core/filters/output/ImageMetadataCopier.cpp
+++ src/core/filters/output/ImageMetadataCopier.cpp
@@ -3,13 +3,19 @@
 #include <exiv2/exiv2.hpp>
 #include <iostream>
 
+#if EXIV2_TEST_VERSION(0,28,0)
+typedef Exiv2::Image::UniquePtr ExivUniquePtr;
+#else
+typedef Exiv2::Image::AutoPtr ExivUniquePtr;
+#endif
+
 bool
 ImageMetadataCopier::iccProfileDefined(const QString& filename)
 {
 #ifdef _WIN32
-    const Exiv2::Image::AutoPtr src_image = Exiv2::ImageFactory::open(filename.toStdWString());
+    const ExivUniquePtr src_image = Exiv2::ImageFactory::open(filename.toStdWString());
 #else
-    const Exiv2::Image::AutoPtr src_image = Exiv2::ImageFactory::open(filename.toStdString());
+    const ExivUniquePtr src_image = Exiv2::ImageFactory::open(filename.toStdString());
 #endif
     if (src_image.get()) {
         src_image->readMetadata();
@@ -25,9 +31,9 @@ ImageMetadataCopier::copyMetadata(const QString& src_img,
     try
     {
 #ifdef _WIN32
-		const Exiv2::Image::AutoPtr src_image = Exiv2::ImageFactory::open(src_img.toStdWString());
+		const ExivUniquePtr src_image = Exiv2::ImageFactory::open(src_img.toStdWString());
 #else
-		const Exiv2::Image::AutoPtr src_image = Exiv2::ImageFactory::open(src_img.toStdString());
+		const ExivUniquePtr src_image = Exiv2::ImageFactory::open(src_img.toStdString());
 #endif
 
         if (src_image.get()) {
@@ -36,12 +42,17 @@ ImageMetadataCopier::copyMetadata(const QString& src_img,
             if (src_image->iccProfileDefined()) {
                 src_image->iccProfile();
 #ifdef _WIN32
-				const Exiv2::Image::AutoPtr dst_image = Exiv2::ImageFactory::open(dst_img.toStdWString());
+				const ExivUniquePtr dst_image = Exiv2::ImageFactory::open(dst_img.toStdWString());
 #else
-				const Exiv2::Image::AutoPtr dst_image = Exiv2::ImageFactory::open(dst_img.toStdString());
+				const ExivUniquePtr dst_image = Exiv2::ImageFactory::open(dst_img.toStdString());
 #endif
                 dst_image->readMetadata();
+#if EXIV2_TEST_VERSION(0,28,0)
+                Exiv2::DataBuf profile = src_image->iccProfile();
+                dst_image->setIccProfile(std::move(profile));
+#else
                 dst_image->setIccProfile(*src_image->iccProfile());
+#endif
                 dst_image->writeMetadata();
                 return true;
             }
