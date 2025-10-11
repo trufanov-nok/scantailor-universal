65116dae440ea020372b2dce95654da998c70817
mpcgd <>
2025-10-11 20:40:49 +0200

    fix(PdfReader): replace pdf_drop_page with fz_drop_page
    Updated function calls in PdfReader.cpp to use fz_drop_page instead of pdf_drop_page, with proper casting of pdf_page to (fz_page*) to align with MuPDF API and ensure correct resource management.

--- src/core/PdfReader.cpp
+++ src/core/PdfReader.cpp
@@ -99,7 +99,7 @@ static bool pageHasAnnotations(fz_context* ctx, fz_document* doc, int page_num) 
         pdf_annot* annot = pdf_first_annot(ctx, pdf_page);
         bool hasAnnotations = (annot != NULL);
 
-        pdf_drop_page(ctx, pdf_page);
+        fz_drop_page(ctx, (fz_page*)pdf_page);
         return hasAnnotations;
     } catch (...) {
         return false;
@@ -571,7 +571,7 @@ PdfReader::extractEmbeddedImages(QIODevice& device, int page_num)
 #ifdef DEBUG
             qDebug() << "No resources found on page" << page_num;
 #endif
-            pdf_drop_page(ctx, pdf_page);
+            fz_drop_page(ctx, (fz_page*)pdf_page);
             fz_drop_document(ctx, doc);
             fz_drop_stream(ctx, stream);
             fz_drop_context(ctx);
@@ -584,7 +584,7 @@ PdfReader::extractEmbeddedImages(QIODevice& device, int page_num)
 #ifdef DEBUG
             qDebug() << "No XObject dictionary found on page" << page_num;
 #endif
-            pdf_drop_page(ctx, pdf_page);
+            fz_drop_page(ctx, (fz_page*)pdf_page);
             fz_drop_document(ctx, doc);
             fz_drop_stream(ctx, stream);
             fz_drop_context(ctx);
@@ -660,7 +660,7 @@ PdfReader::extractEmbeddedImages(QIODevice& device, int page_num)
         qDebug() << "Total embedded images found:" << images.size();
 #endif
 
-        pdf_drop_page(ctx, pdf_page);
+        fz_drop_page(ctx, (fz_page*)pdf_page);
         fz_drop_document(ctx, doc);
         fz_drop_stream(ctx, stream);
         fz_drop_context(ctx);
