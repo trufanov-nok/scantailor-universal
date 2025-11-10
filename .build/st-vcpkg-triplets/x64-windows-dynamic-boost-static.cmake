a8f9f7064bdabb74264163abb3383d60ec3329d0
Alexander Trufanov <trufanovan@gmail.com>
2025-11-10 13:15:09 +0300

    Update2

---
+++ .build/st-vcpkg-triplets/x64-windows-dynamic-boost-static.cmake
@@ -0,0 +1,7 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+
+if (${PORT} MATCHES "boost*")
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif ()
